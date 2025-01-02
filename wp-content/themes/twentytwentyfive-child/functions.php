18ae85c04b4826485bd1d19fddb10424e7fd9205
Ajayjani8008 <ajaykumar931396@gmail.com>
2025-01-02 16:17:52 +0530

    remove  woocommerce  updatetion

--- wp-content/themes/twentytwentyfive-child/functions.php
+++ wp-content/themes/twentytwentyfive-child/functions.php
@@ -1,18 +1,21 @@
 <?php
-// Enqueue parent and child theme styles
-function twentytwentyfive_child_enqueue_styles() {
-    // Enqueue parent theme style
+function twentytwentyfive_child_enqueue_styles()
+{
     wp_enqueue_style(
-        'twentytwentyfive-parent-style',
+        'parent-style',
         get_template_directory_uri() . '/style.css'
     );
 
-    // Enqueue child theme style
     wp_enqueue_style(
-        'twentytwentyfive-child-style',
+        'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        ['twentytwentyfive-parent-style']
+        ['parent-style']
     );
+
 }
 add_action('wp_enqueue_scripts', 'twentytwentyfive_child_enqueue_styles');
-?>
+
+add_action( 'after_setup_theme', function() {
+    add_theme_support( 'woocommerce' );
+});
+
